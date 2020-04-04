61dc5b85279828147a9a73fa66b82fbba53d12ce
yuuta66jp <yuuta66jp@gmail.com>
2020-04-04 13:31:57 +0000

    [start]JavaScript基本文法

--- js/main.js
+++ js/main.js
@@ -1,35 +1,15 @@
-'use strict';
+'user strict';
 
-{
-    const btn = document.getElementById('btn');
+// 定数 const
+// 変数 let var
 
-    btn.addEventListener('click', () => {
-        // const results = ['大吉', '中吉', '凶', '末吉'];
-        // const results = ['大吉', '大吉', '大吉', '凶'];
-        // const n = Math.floor(Math.random() * results.length);
-        // btn.textContent = results[n];
-        // btn.textContent = results[Math.floor(Math.random() * results.length)];
-        const n = Math.random();
-        if (n < 0.05) {
-          btn.textContent = '大吉'; // 5%
-        } else if (n < 0.2) {
-          btn.textContent = '中吉'; // 15%
-        } else {
-          btn.textContent = '凶'; // 80%
-        }
 
-        // btn.textContent = n;
+let price = 150;
 
-        // switch (n) {
-        //     case 0:
-        //         btn.textContent = '大吉';
-        //         break;
-        //     case 1:
-        //         btn.textContent = '中吉';
-        //         break;
-        //     case 2:
-        //         btn.textContent = '凶';
-        //         break;
-        // }
-    });
-}
+console.log(price * 140);
+console.log(price * 160);
+
+price = 170;
+
+console.log(price * 140);
+console.log(price * 160);
