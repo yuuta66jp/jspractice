580d4e5f5132c0c6d73a6d5862101cf461bd19ad
yuuta66jp <yuuta66jp@gmail.com>
2020-04-05 12:36:24 +0000

    [finish]JavaScript基本文法

--- js/main.js
+++ js/main.js
@@ -1,16 +1,29 @@
 'user strict';
 
-function showAd(message = 'Ad') {
-  console.log('----------')
-  console.log(`----${message}----`)
-  console.log('----------')
-}
+// const sum = function(a, b, c) {
+//   return a + b + c;
+// };
 
-showAd('Header Ad');
-console.log('Tom is great!')
-console.log('Bob is great!')
-// showAd('Ad');
-showAd();
-console.log('Steve is great!')
-console.log('Richard is great!')
-showAd('Footer Ad');
+// const sum = (a, b, c) => a + b + c;
+
+// const total = sum(1, 2, 3) + sum(3, 4, 5);
+// console.log(total)
+// const double = function(a) {
+//   return a * 2;
+// };
+// const double = a => a * 2;
+// console.log(double(12));
+
+// const x = 2;
+
+// function f() {
+//   // const x = 1;
+//   console.log(x);
+// }
+
+// f();
+// console.log(x);
+{
+  const x = 100;
+  console.log(x);
+}
