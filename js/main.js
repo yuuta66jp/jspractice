cc7b56d7b8c883b638e2790927ed598e4b611536
yuuta66jp <yuuta66jp@gmail.com>
2020-04-07 14:08:54 +0000

    [update]JavaScriptオブジェクト編2

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 'user strict';
 
 {
-  const d = [2019, 11, 14];
+  let i = 0;
+  function showTime() {
+    console.log(new Date());
+    const timeoutId = setTimeout(showTime, 1000);
+    i++;
+    if(i > 2) {
+      clearTimeout(timeoutId);
+    }
+  }
 
-  // console.log(d.join('/'));
-
-  const t = '17:08:24';
-  // console.log(t.split(':'));
-  const [hour, minute, second] = t.split(':');
-  console.log(hour);
-  console.log(minute);
-  console.log(second);
+  showTime();
 }
